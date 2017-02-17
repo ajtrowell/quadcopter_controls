0f3eb915eb02dd170872c528b42460ff7e9f46a2
AJTrowell <ajtrowell@gmail.com>
2017-02-17 14:07:01 -0600

    Quadcopter: vel,acel,mass,netForce. Add physics object
    Intent is to model 'real' physics, so units are in 
    meters and seconds.  Consequently, physics 
    object provides conversions from 'frames' to 
    'simulationSeconds' andfrom 'pixels' to 
    'meters'.
    
    Quadcopter class:
    Added more physical state properties, so now we 
    have position, velocity, acceleration, currentThurst, 
    netForce, and mass.
    Added thrust() method to set the thrust level 
    of the craft, given as a p5.Vector.
    Added update() method to perform propogate state 
    Calculate
    netForce <== thrust & gravity
    acceleration <== netForce & mass
    velocity <== acceleration, timestep, and past velocity
    position <== velocity, timestep, pxToMeters, and 
    past position.
    
    Note that all are in units of meters and secondsd 
    except position, which is in units of pixels.

--- Quadcopter.js
+++ Quadcopter.js
@@ -1,10 +1,51 @@
 // Quadcopter constructor
 function Quadcopter(){
-    this.pos = createVector(width/2,height/2);
-    this.size = createVector(width/5,width/30);
-}
+    this.size_pixels = createVector(width/5,width/30);
+    this.position_pixels = createVector(width/2,height/2);
+    this.velocity_mps = createVector(0,0); // In meters per second
+    this.acceleration_mpss = createVector(0,0); // In meters per second per second.
+    
+    //Physical Properties
+    this.mass_kg = 100; //kg
+    this.maxThrust_newtons = 5000; // Newtons of force.
+
+    //NextState
+    this.netForce_newtons = createVector(0, 0); // Initial net force.
+    this.thrust_newtons = createVector(0,0); //Initial applied thrust.
+    
+}//constructor
 Quadcopter.prototype.draw = function(){
     fill(100);
-    rect(this.pos.x - this.size.x/2, this.pos.y - this.size.y/2, 
-         this.size.x, this.size.y);
-}
+    rect(this.position_pixels.x - this.size_pixels.x/2, 
+         this.position_pixels.y - this.size_pixels.y/2, 
+         this.size_pixels.x, this.size_pixels.y);
+
+    // Draw circular 'pin' at C.G. to show rotation point.
+    fill(200,10,10);
+    ellipse(this.position_pixels.x, this.position_pixels.y, 
+            this.size_pixels.y, this.size_pixels.y);
+}//draw()
+Quadcopter.prototype.update = function(){
+    //Update netForce from gravity and thrust
+    //Create gravityVector with units of Newtons:
+    var gravityVector = createVector(0,this.mass_kg * physics.gravity_mps);
+    this.netForce_newtons = p5.Vector.add(this.thrust_newtons,gravityVector);
+    // Update acceleration from netForce and mass
+    //  acceleration  = force(Newtons) / mass(Kilograms)
+    this.acceleration_mpss = this.netForce_newtons.copy().div(this.mass_kg);
+
+    // Update velocity from acceleration
+    // Convert timestep based on physics.framesPerSimSecond.
+
+    // Update position from velocity.
+    // Take note of physics.pixelsPerMeter
+    // Convert timestep based on physics.framesPerSimSecond.
+    // ??? is this.position_pixels meant to be in pixels or in meters?
+    
+
+    
+}//update()
+Quadcopter.prototype.thrust = function(thrustVector){
+    //Set current applied thrust as Vector object.
+    this.thrust_newtons = thrustVector.copy();
+}//thrust()
--- sketch.js
+++ sketch.js
@@ -1,13 +1,19 @@
 var quad1;
+var physics = {
+    gravity_mps: 10, // meters/sec^2
+    pixelsPerMeter: 2,
+    framesPerSimSecond: 30 
+}
 
 function setup() {
     createCanvas(windowWidth,windowHeight);
     quad1 = new Quadcopter();
-
 }
 
 function draw() {
+    physics.framesPerSimSecond = getFrameRate(); // Seems redundant
     background(0);
+    quad1.update();
     quad1.draw();
 }
 
