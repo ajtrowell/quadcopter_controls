f2c25adb038b37096d9a37025d13a6442135ae30
AJTrowell <ajtrowell@gmail.com>
2017-02-17 15:53:22 -0600

    physics: Added setTimeScale as method.
    setPhysicsTimeScale makes more sense as a 
    method of the physics object.

--- sketch.js
+++ sketch.js
@@ -1,10 +1,5 @@
-var quad1;
-var physics = {
-    gravity_mps: 10, // meters/sec^2
-    pixelsPerMeter: 10,
-    framesPerSimSecond: 30,  // Initial value.
-    timeScale : 1 // Larger runs faster.
-}
+var quad1; // Quadcopter object instance.
+var physics;
 
 function setup() {
     createCanvas(windowWidth,windowHeight);
@@ -12,28 +7,33 @@ function setup() {
 }
 
 function draw() {
-    setPhysicsTimeScale();
+    physics.setTimeScale(); // Update based on current fps.
     background(0);
     quad1.update();
     quad1.draw();
 }
 
 
+physics = {
+    gravity_mps: 10, // meters/sec^2
+    pixelsPerMeter: 10,
+    framesPerSimSecond: 30,  // Initial value.
+    timeScale : 1, // Larger runs faster.
 
-
-setPhysicsTimeScale = function() {
-    // Update timescale as FPS changes.
-    // Prevent having a arbitrarily small timescale.
-    // Note: this function should probably be a member of the 
-    // physics object.
-    var tempFPS = getFrameRate();
-    var minFPS = 10;
-    
-    if (tempFPS >= minFPS) {
-        physics.framesPerSimSecond = tempFPS / physics.timeScale;
-    } else {
-        physics.framesPerSimSecond = minFPS;
-    }
-}
+    setTimeScale : function() {
+        // Update timescale as FPS changes.
+        // Prevent having a arbitrarily small timescale.
+        // Note: this function should probably be a member of the 
+        // physics object.
+        var tempFPS = getFrameRate();
+        var minFPS = 10;
+        
+        if (tempFPS >= minFPS) {
+            this.framesPerSimSecond = tempFPS / this.timeScale;
+        } else {
+            this.framesPerSimSecond = minFPS;
+        }
+    }//setPhysicsTimeScale()
+} //physics object
 
 
